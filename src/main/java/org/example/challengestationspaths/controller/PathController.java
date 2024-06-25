20a2068c71d745fb0c9115988047a40f7174f068
Juan A. Tilcara <jatilcara1988@gmail.com>
2024-06-25 17:32:12 -0300

    Se agregan logs y se corrigen tipos de respuesta.

--- src/main/java/org/example/challengestationspaths/controller/PathController.java
+++ src/main/java/org/example/challengestationspaths/controller/PathController.java
@@ -7,18 +7,20 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.challengestationspaths.dto.request.PathDTO;
 import org.example.challengestationspaths.dto.response.ResponseDTO;
 import org.example.challengestationspaths.exception.CustomHttpException;
-import org.example.challengestationspaths.model.Path;
 import org.example.challengestationspaths.service.PathService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "Path Controller", description = "Controller to add Path, and search the optimal route.")
 @RestController
 @RequestMapping("/paths")
 public class PathController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PathController.class);
 
     @Autowired
     private PathService service;
@@ -30,16 +32,18 @@ public class PathController {
             @ApiResponse(responseCode = "200", description = "Add is ok"),
             @ApiResponse(responseCode = "400", description = "Bad Request, validate source_id and destination_id, and exist errors, do not insert")
     })
-    @PutMapping("/{path_id}")
-    public ResponseDTO updatePath(@PathVariable(name = "path_id") Long pathId,
-                                  @RequestBody PathDTO request) {
+    @RequestMapping(value = "/{path_id}", consumes = "application/json", produces = "application/json", method = RequestMethod.PUT)
+    public ResponseEntity<ResponseDTO> updatePath(@PathVariable(name = "path_id") Long pathId,
+                                     @RequestBody PathDTO request) {
+        LOGGER.info("Request to update path with path_id {}", pathId);
         try {
             service.addPath(pathId, request);
-            return new ResponseDTO(HttpStatus.OK.name());
+            return ResponseEntity.ok(new ResponseDTO(HttpStatus.OK.name()));
         } catch (CustomHttpException e) {
-            return new ResponseDTO(e.getStatus().name() + " - " + e.getMessage());
+            return new ResponseEntity<>(new ResponseDTO(e.getStatus().name() + " - " + e.getMessage()), e.getStatus());
         } catch (Exception e) {
-            return new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.name());
+            logException(e);
+            return new ResponseEntity<>(new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.name()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -50,15 +54,17 @@ public class PathController {
             @ApiResponse(responseCode = "200", description = "Ok, when the optimal route is find"),
             @ApiResponse(responseCode = "400", description = "Bad Request, when error with source_id or destination_id")
     })
-    @GetMapping("/{source_id}/{destination_id}")
-    public Object getShortestPath(@PathVariable(name = "source_id") Long sourceId,
+    @RequestMapping(value = "/{source_id}/{destination_id}", produces = "application/json", method = RequestMethod.GET)
+    public ResponseEntity<?> getShortestPath(@PathVariable(name = "source_id") Long sourceId,
                                            @PathVariable(name = "destination_id") Long destinationId) {
+        LOGGER.info("Get optimal route with source_id {} and destination_id {}", sourceId, destinationId);
         try {
-            return service.getPaths(sourceId, destinationId);
+            return ResponseEntity.ok(service.getPaths(sourceId, destinationId));
         } catch (CustomHttpException e) {
-            return new ResponseDTO(e.getStatus().name() + " - " + e.getMessage());
+            return new ResponseEntity<>(new ResponseDTO(e.getStatus().name() + " - " + e.getMessage()), e.getStatus());
         } catch (Exception e) {
-            return new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.name());
+            logException(e);
+            return new ResponseEntity<>(new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.name()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -69,9 +75,24 @@ public class PathController {
             @ApiResponse(responseCode = "200", description = "Ok, search return data"),
             @ApiResponse(responseCode = "404", description = "Not Found, the search return empty data")
     })
-    @GetMapping()
-    public List<Path> getAllPaths() {
-        return service.getAllPaths();
+    @RequestMapping(produces = "application/json", method = RequestMethod.GET)
+    public ResponseEntity<?> getAllPaths() {
+        LOGGER.info("Search all Path's");
+        try {
+            return ResponseEntity.ok(service.getAllPaths());
+        } catch (CustomHttpException e) {
+            return new ResponseEntity<>(new ResponseDTO(e.getStatus().name() + " - " + e.getMessage()), e.getStatus());
+        } catch (Exception e) {
+            logException(e);
+            return new ResponseEntity<>(new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.name()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    private void logException(Exception e) {
+        LOGGER.error(e.getMessage());
+        if (e.getMessage() == null) {
+            LOGGER.warn("The error message is null, printstacktrace to obtain more information...");
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/example/challengestationspaths/controller/StationController.java
+++ src/main/java/org/example/challengestationspaths/controller/StationController.java
@@ -7,18 +7,20 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.challengestationspaths.dto.request.StationDTO;
 import org.example.challengestationspaths.dto.response.ResponseDTO;
 import org.example.challengestationspaths.exception.CustomHttpException;
-import org.example.challengestationspaths.model.Station;
 import org.example.challengestationspaths.service.StationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "Station Controller", description = "Controller to add station")
 @RestController
 @RequestMapping("/stations")
 public class StationController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StationController.class);
 
     @Autowired
     private StationService service;
@@ -30,16 +32,18 @@ public class StationController {
             @ApiResponse(responseCode = "200", description = "Add is ok"),
             @ApiResponse(responseCode = "400", description = "Bad Request, the same name do not insert")
     })
-    @PutMapping(value = "/{station_id}", produces = "application/json")
-    public ResponseDTO updateStation(@PathVariable(value = "station_id") Long stationId,
-                                  @RequestBody StationDTO request) {
+    @RequestMapping(value = "/{station_id}", produces = "application/json", consumes = "application/json", method = RequestMethod.PUT)
+    public ResponseEntity<ResponseDTO> updateStation(@PathVariable(value = "station_id") Long stationId,
+                                        @RequestBody StationDTO request) {
+        LOGGER.info("Received request to update station with id {} and name {}", stationId, request.getName());
         try {
             service.addStation(stationId, request.getName());
-            return new ResponseDTO(HttpStatus.OK.name());
+            return ResponseEntity.ok(new ResponseDTO(HttpStatus.OK.name()));
         } catch (CustomHttpException e) {
-            return new ResponseDTO(e.getStatus().name() + " - " + e.getMessage());
+            return new ResponseEntity<>(new ResponseDTO(e.getStatus().name() + " - " + e.getMessage()), e.getStatus());
         } catch (Exception e) {
-            return new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.name());
+            logException(e);
+            return new ResponseEntity<>(new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.name()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -50,9 +54,25 @@ public class StationController {
             @ApiResponse(responseCode = "200", description = "Ok, search return data"),
             @ApiResponse(responseCode = "404", description = "Not Found, the search return empty data")
     })
-    @GetMapping
-    public List<Station> getAllStations() {
-        return service.getAllStations();
+    @RequestMapping(produces = "application/json", method = RequestMethod.GET)
+    public ResponseEntity<?> getAllStations() {
+        LOGGER.info("Search all Station's");
+        try {
+            return ResponseEntity.ok(service.getAllStations());
+        } catch (CustomHttpException e) {
+            return new ResponseEntity<>(new ResponseDTO(e.getStatus().name() + " - " + e.getMessage()), e.getStatus());
+        } catch (Exception e) {
+            logException(e);
+            return new ResponseEntity<>(new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.name()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void logException(Exception e) {
+        LOGGER.error(e.getMessage());
+        if (e.getMessage() == null) {
+            LOGGER.warn("The error message is null, printstacktrace to obtain more information...");
+            e.printStackTrace();
+        }
     }
 
 }
